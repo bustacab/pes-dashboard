a7789954e2322b302a4321b384886367ff0a90cf
Franco Giaco <giaco1997@hotmail.com>
2022-01-20 18:54:38 -0300

    Crear jugadores y crear equipo

--- backend/db.py
+++ backend/db.py
@@ -8,12 +8,31 @@ def create_user(nombre):
         user="pes",
         password="pes"
     )
-    cursor = connection.cursor()
+    cursor = connection.cursor() #Cursor para ingresar querys
     cursor.execute(f"INSERT INTO player (nombre) VALUES ({nombre})")
 
+def create_team(estadio, nombre):
+    connection = psycopg2.connect(
+        host="127.0.0.1",
+        database="pes",
+        user="pes",
+        password="pes"
+    )
+    cursor = connection.cursor()
+    cursor.execute(f"INSERT INTO equipo(estadio, nombre) VALUES ({estadio, nombre})")
+
+def create_players(nombre, edad):
+    connection = psycopg2.connect(
+        host="127.0.0.1",
+        database="pes",
+        user="pes",
+        password="pes"
+    )
+    cursor = connection.cursor()
+    cursor.execute(f"INSERT INTO jugadores(nombre, edad) VALUES ({nombre}, {edad})")
 
 def connect_to_db():
-    connection = psycopg2.connect(
+    connection = psycopg2.connect( #Conexion con db utilizando modulo psycopg2 la funcion connect
         host="127.0.0.1",
         database="pes",
         user="pes",
@@ -24,5 +43,6 @@ def connect_to_db():
     partidos = cursor.fetchall()
     for partido in partidos:
         print(partido)
+
 
 connect_to_db()
